366ac19e0f540c59651a558daf59182898b65349
Shiva Prasad <Shivaprasadgunaganti77@gmail.com>
2026-06-18 15:48:00 +0530

    bugs

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,31 +1,8 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
-// export default function ProtectedRoute({ children }) {
-//   const { user, loading, isGuest  } = useAuth();
-
-//   if (loading) return null;
-
-//   // if (!user) {
-//   //   return <Navigate to="/login" replace />;
-//   // }
-
-//   if (!user && !isGuest) {
-//   return <Navigate to="/login" replace />;
-// }
-
-//   return children;
-// }
-
-
 export default function ProtectedRoute({ children }) {
   const { user, loading, isGuest } = useAuth();
-
-  // console.log("ProtectedRoute", {
-  //   user,
-  //   loading,
-  //   isGuest,
-  // });
 
   if (loading) return null;
 
