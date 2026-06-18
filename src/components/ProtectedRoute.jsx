dcbb9bfeb9a0478ddf988c6ce2385e68f03eda7b
Shiva Prasad <Shivaprasadgunaganti77@gmail.com>
2026-06-18 16:17:48 +0530

    merge

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,11 +4,6 @@ import { useAuth } from "../context/AuthContext";
 export default function ProtectedRoute({ children }) {
   const { user, loading, isGuest } = useAuth();
 
-<<<<<<< HEAD
-  
-
-=======
->>>>>>> 366ac19e0f540c59651a558daf59182898b65349
   if (loading) return null;
 
   if (!user && !isGuest) {
