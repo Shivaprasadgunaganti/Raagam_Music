2e42b7622ce708cad9eb03c51c27829579a6dc25
Shivaprasadgunaganti <shivaprasad06559@gmail.com>
2026-08-12 23:15:04 +0530

    Configure Supabase environment variables

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,11 +1,24 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://cxgnppogmhtagctqcgdp.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4Z25wcG9nbWh0YWdjdHFjZ2RwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0ODcwMTQsImV4cCI6MjA4MDA2MzAxNH0.h6PhOZoVVTpvY4kRy7_Ihvr-dQ-bwpHzic4poynF1uE";
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
 
+
+// import { createClient } from "@supabase/supabase-js";
+
+// const SUPABASE_URL = "https://cxgnppogmhtagctqcgdp.supabase.co";
+// const SUPABASE_ANON_KEY =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4Z25wcG9nbWh0YWdjdHFjZ2RwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0ODcwMTQsImV4cCI6MjA4MDA2MzAxNH0.h6PhOZoVVTpvY4kRy7_Ihvr-dQ-bwpHzic4poynF1uE";
+
+// export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+
+// 
 
 // import { createClient } from "@supabase/supabase-js";
 
